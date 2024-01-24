0cc2b292e312df932d1ec730f868fedb8be9fdb6
JinKyu <rlawlsrb167@naver.com>
2024-01-24 18:47:38 +0900

    Style: Header 스타일링

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import styles from "./Header.module.css";
+import { LuUserCircle2 } from "react-icons/lu";
 export default function Header() {
+  const [userMenuVisible, setUserMenuVisible] = useState(false);
   return (
-    <header>
-      <div>
-        <h1>
+    <header className={styles.header}>
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>
           <Link to="/">Samsung</Link>
         </h1>
-        <div>
-          <ul>
-            <li>스마트폰</li>
-            <li>태블릿</li>
-            <li>갤럭시북</li>
-            <li>워치</li>
-            <li>버즈</li>
-            <li>고객지원</li>
+        <div className={styles.nav}>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}>스마트폰</li>
+            <li className={styles.menuItem}>태블릿</li>
+            <li className={styles.menuItem}>갤럭시북</li>
+            <li className={styles.menuItem}>워치</li>
+            <li className={styles.menuItem}>버즈</li>
+            <li className={styles.menuItem}>고객지원</li>
           </ul>
-          <div>
-            <em></em>
-            <ul>
-              <li>로그인</li>
-              <li>장바구니</li>
+          <div className={styles.user}>
+            <LuUserCircle2
+              className={styles.userIcon}
+              onClick={() => setUserMenuVisible((prev) => !prev)}
+            />
+            <ul
+              className={`${styles.userMenu} ${
+                userMenuVisible === true && styles.show
+              }`}
+            >
+              <li className={styles.userMenuItem}>로그인</li>
+              <li className={styles.userMenuItem}>장바구니</li>
             </ul>
           </div>
         </div>
