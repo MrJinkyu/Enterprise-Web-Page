74371e0daf1fb43895665b6db39a38fa9829ff78
JinKyu <rlawlsrb167@naver.com>
2024-01-24 16:05:27 +0900

    Feat: Header 마크업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        <h1>
+          <Link to="/">Samsung</Link>
+        </h1>
+        <div>
+          <ul>
+            <li>스마트폰</li>
+            <li>태블릿</li>
+            <li>갤럭시북</li>
+            <li>워치</li>
+            <li>버즈</li>
+            <li>고객지원</li>
+          </ul>
+          <div>
+            <em></em>
+            <ul>
+              <li>로그인</li>
+              <li>장바구니</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
