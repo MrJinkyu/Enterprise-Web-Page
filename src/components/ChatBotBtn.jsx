6902a80689ba8cbca4fe23530cd957e438b409c0
JinKyu <rlawlsrb167@naver.com>
2024-02-08 01:38:29 +0900

    Style: ChatBotBtn + Modal 반응형 작업

--- src/components/ChatBotBtn.jsx
+++ src/components/ChatBotBtn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./ChatBotBtn.module.css";
 export default function ChatBotBtn({ handleClick }) {
   return (
-    <button className={styles.btn} onClick={() => handleClick(true)}>
+    <button className={styles.btn} onClick={() => handleClick((prev) => !prev)}>
       <img
         className={styles.img}
         src="https://res.cloudinary.com/dew8mkvyz/image/upload/v1699336797/mxo2gd6fbjexsugjodip.png"
