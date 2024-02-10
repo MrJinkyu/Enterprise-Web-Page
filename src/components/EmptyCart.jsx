cfed0fa0e4b336be2fe55476fed436ab1ba0fe01
JinKyu <rlawlsrb167@naver.com>
2024-02-10 22:42:12 +0900

    Style: 빈 장바구니 컴포넌트 스타일 변경 및 반응형 작업

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import styles from "./EmptyCart.module.css";
-import { useNavigate } from "react-router-dom";
-import { MdOutlineNavigateNext } from "react-icons/md";
-
+import { MdLocalShipping } from "react-icons/md";
 export default function EmptyCart() {
-  const navigate = useNavigate();
   return (
     <section className={styles.container}>
       <div className={styles.info}>
@@ -12,25 +9,8 @@ export default function EmptyCart() {
         <div className={styles.resultShipping}>
           모든 주문에 무료 배송 서비스가 제공됩니다.
         </div>
-      </div>
-      <div className={styles.smartphone}>
-        <div className={styles.smartphoneWrapper}>
-          <div className={styles.text}>
-            <p className={styles.name}>Galaxy S24 Ultra</p>
-            <p className={styles.description}>
-              의사소통하는 가장 쉬운 방법,<br></br>
-              실시간 통역
-            </p>
-            <button
-              onClick={() =>
-                navigate("/products", { state: { menu: "스마트폰" } })
-              }
-              className={styles.btn}
-            >
-              <span className={styles.btnText}>더 알아보기</span>
-              <MdOutlineNavigateNext className={styles.btnIcon} />
-            </button>
-          </div>
+        <div className={styles.iconBox}>
+          <MdLocalShipping className={styles.icon} />
         </div>
       </div>
     </section>
